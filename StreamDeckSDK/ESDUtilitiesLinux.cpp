79c13e5a8fd68a4d6cea0b16d3f04e641df9f848
Fred Emmott <fred@fredemmott.com>
2020-06-19 21:27:37 -0700

    Rename ESDUtilitiesStubs to ESDUtilitiesLinux

--- StreamDeckSDK/ESDUtilitiesLinux.cpp
+++ StreamDeckSDK/ESDUtilitiesLinux.cpp
@@ -2,9 +2,6 @@
 //      This source code is licensed under the MIT-style license found in the
 // LICENSE file.
 
-// This file contains always-failing versions of ESDUtilities, which should
-// build on any platform.
-
 #include "ESDUtilities.h"
 
 std::string ESDUtilities::GetPluginExecutablePath() {
