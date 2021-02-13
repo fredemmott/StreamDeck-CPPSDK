be2da199f306f0e464dc1abe80e77b444cb707ab
Fred Emmott <fred@fredemmott.com>
2021-02-13 12:25:34 -0600

    Update to latest version of asio

--- Vendor/asio.cmake
+++ Vendor/asio.cmake
@@ -2,11 +2,13 @@ include(ExternalProject)
 
 ExternalProject_Add(
   asio_source
-  URL https://github.com/chriskohlhoff/asio/archive/asio-1-16-1.tar.gz
-  URL_HASH SHA512=7e5f8a503b6e8e939b3e77921bea2a11312dbe2ec609669c387ff11ebb97c2fbba96a57d064b34946b3db2cd45de6524a39d3050fd5b7d5b7f4fb595848a27ed
+  URL https://github.com/chriskohlhoff/asio/archive/asio-1-18-1.tar.gz
+  URL_HASH SHA512=c84e6fca448ed419a976756840f3f4543291a5a7d4f62d4de7c06945b2cd9ececca6633049ad5e36367d60f67a4f2735be017445514ae9fa9497d4af2a4d48f8
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
+  PATCH_COMMAND
+    git init && git apply --ignore-whitespace "${CMAKE_CURRENT_LIST_DIR}/asio-7377f941cc9831c4c3cba1e71d3bc29bddf1a700.patch"
 )
 ExternalProject_Get_Property(asio_source SOURCE_DIR)
 add_library(asio INTERFACE)
@@ -16,19 +18,8 @@ target_compile_definitions(
   INTERFACE
   ASIO_STANDALONE=1
 )
-
 target_include_directories(
   asio
   INTERFACE
   ${SOURCE_DIR}/asio/include
-)
-
-install(
-	DIRECTORY
-  ${SOURCE_DIR}/asio/include/
-	DESTINATION
-  "${CMAKE_INSTALL_INCLUDEDIR}"
-  FILES_MATCHING
-  PATTERN "*.hpp"
-  PATTERN "*.ipp"
 )
