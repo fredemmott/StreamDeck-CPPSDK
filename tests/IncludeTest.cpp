d94505f6a1d3b06590881184f0d5ce9bdf8c95c6
Fred Emmott <fred@fredemmott.com>
2021-02-19 11:49:31 -0600

    add IncludeTest
    At least makes sure there's no obvious errors.

---
+++ tests/IncludeTest.cpp
@@ -0,0 +1,20 @@
+// @copyright  (c) 2020, Frederick Emmott
+// This source code is licensed under the MIT-style license found in
+// the LICENSE file.
+
+#include "Test.h"
+
+#include <StreamDeckSDK/ESDAction.h>
+#include <StreamDeckSDK/EPLJSONUtils.h>
+#include <StreamDeckSDK/ESDActionWithExternalState.h>
+#include <StreamDeckSDK/ESDBasePlugin.h>
+#include <StreamDeckSDK/ESDConnectionManager.h>
+#include <StreamDeckSDK/ESDLocalizer.h>
+#include <StreamDeckSDK/ESDLogger.h>
+#include <StreamDeckSDK/ESDMain.h>
+#include <StreamDeckSDK/ESDPlugin.h>
+#include <StreamDeckSDK/ESDUtilities.h>
+
+bool test_main() {
+  return true;
+}
